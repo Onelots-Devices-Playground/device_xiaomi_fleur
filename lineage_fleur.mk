8d45409423a669bf9b32d315e59f15597bad618f
Onelots <onelots@onelots.fr>
2025-10-20 01:39:30 +0200

    fleur: Initialize for EvolutionX 10.X
    Signed-off-by: Onelots <onelots@onelots.fr>

--- lineage_fleur.mk
+++ lineage_fleur.mk
@@ -14,6 +14,11 @@ $(call inherit-product, device/xiaomi/fleur/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# EvolutionX flags
+BUILD_BCR := true
+EVO_BUILD_TYPE := Official
+TARGET_USES_MINI_GAPPS := true
+
 PRODUCT_NAME := lineage_fleur
 PRODUCT_DEVICE := fleur
 PRODUCT_MANUFACTURER := Xiaomi
